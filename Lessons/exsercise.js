cc71928e9735d11a356d118817a57a92094ac39f
AlexVjatkin <alexander.vjatkin@gmail.com>
2015-11-02 02:07:39 +0300

    first

--- Lessons/exsercise.js
+++ Lessons/exsercise.js
@@ -1,21 +1,15 @@
 /**
  * Created by alex on 24.10.15.
  */
-function chessTable(x,y) {
-    this.rows = x;
-    this.cols = y;
-    this.getCell = function() {
-
-    };
-    this.setCell = function() {
-
-    };
-    this.createTable = function() {
-        for (i = 0; i< this.rows; i++) {
-            for (j = 0; j < this.cols; j++) {
-                document.writeln("<div>1</div>");
-            }
-        }
-    };
-
-}
+(function($){
+    $(function(){
+        $('body>div')[0].style.cssText = "width : 100%; display: table; border-spacing: 5px";
+        Array.prototype.slice.call($('body > div > div')).forEach(function(elems) {
+            elems.style.cssText = "display: table-cell; width: 40%; background-color: grey";
+        });
+        Array.prototype.slice.call($('div > div > div')).forEach(function(elems) {
+            elems.style.cssText = "float: left;width: 100%; height: 25%; display: table; background-color: cyan";
+        });
+        $("div[id ^='e']").append("<input type='submit'>");
+    });
+})(jQuery);
