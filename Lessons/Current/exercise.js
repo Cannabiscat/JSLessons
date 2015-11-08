ddbdefa68db1654c438874cee87f0b45c80894df
AlexVjatkin <alexander.vjatkin@gmail.com>
2015-11-08 17:56:27 +0300

    jQueryAJAX

--- Lessons/Current/exercise.js
+++ Lessons/Current/exercise.js
@@ -1,3 +1,25 @@
 /**
  * Created by alex on 08.11.15.
  */
+(function($){
+    $(function(){
+        $(document).on('click','.submit', function(event) {
+            $.ajax({
+                url: 'php/validator.php',
+                success: function(data, textStatus) {
+                    console.log(data);
+                },
+                data: {
+                    username: $('input[name=username]').val(),
+                    password: $('input[name=passwd]').val(),
+                    email: $('input[name=email]').val(),
+                    gender: $('input[name=gender]').val(),
+                    credit_card: $('input[name=credit_card]').val(),
+                    bio: $('input[name=bio]').val()
+                },
+                type: 'POST'
+            });
+            event.preventDefault();
+        });
+    });
+})(jQuery);
